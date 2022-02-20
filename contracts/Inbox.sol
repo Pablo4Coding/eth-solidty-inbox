1665f1a56ec4a1e43aa029da225e190936025df4
Pablo Andres <devpabloandres@gmail.com>
2022-02-20 10:05:36 +0100

    feat(compile): scaffold for smart contract compilation

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,4 +1,20 @@
 pragma solidity ^0.4.17;
-// linter warnings (red underline) about pragma version can igonored!
 
-// contract code will go here
+contract Inbox {
+    string public message;
+
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+    function doMath(int a, int b) {
+        a + b;
+        b - a;
+        a * b;
+        a == 0;
+    }
+}
