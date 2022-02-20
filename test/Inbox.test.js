e837c1eb681d4c9c414afcb9c251c2f974e1930a
Pablo Andres <devpabloandres@gmail.com>
2022-02-20 11:16:45 +0100

    test(inbox): first tests implemented

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,37 @@
-// contract test code will go here
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
+
+let accounts;
+let inbox;
+const INITIAL_MESSAGE = "Hi There!";
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // Use one of those accounts to deploy the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_MESSAGE] })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it("has a default message", async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, INITIAL_MESSAGE);
+  });
+
+  it("can change the message", async () => {
+    const newMessage = "New message!";
+    await inbox.methods.setMessage(newMessage).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, newMessage);
+  });
+});
